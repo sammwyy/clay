c2050338cb7c0bc084c75a25445f241d4ca41e7c
sammwy <sammwy.dev@gmail.com>
2026-08-23 15:10:51 -0300

    chore: bump version to 0.0.2

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define CLAY_VERSION "0.0.1"
+#define CLAY_VERSION "0.0.2"
 
 static int require_provider(ClayCommands *commands) {
     clay_sayc(CLAY_YELLOW, "Connect a provider to continue.");
